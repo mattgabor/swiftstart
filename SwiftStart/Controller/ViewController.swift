888e1f20a2a9958123c4818bcfb5ae1a3264fa1c
Matt Gabor <mattgabor@gmail.com>
2016-05-29 21:39:28 -0700

    Created MVC Folders

---
+++ SwiftStart/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by Matthew Gabor on 5/28/16.
+//  Copyright © 2016 mattgabor. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    navigationController?.navigationBar.backgroundColor = UIColor.blueColor()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+
+  }
+}
---
+++ SwiftStart/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  SwiftStart
+//
+//  Created by Matthew Gabor on 5/29/16.
+//  Copyright © 2016 mattgabor. All rights reserved.
+//
+
+import Foundation
