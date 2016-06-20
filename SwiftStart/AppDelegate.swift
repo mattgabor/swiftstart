315cda637fec503d2272dbae82fa662fcab21105
Matt Gabor <mattgabor@gmail.com>
2016-06-19 18:11:22 -0700

    Coverted to swift3 syntax

--- SwiftStart/AppDelegate.swift
+++ SwiftStart/AppDelegate.swift
@@ -16,29 +16,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
 
-  func application(application: UIApplication, didFinishLaunchingWithOptions
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions
     launchOptions: [NSObject: AnyObject]?) -> Bool {
     FIRApp.configure()
     return true
   }
 
-  func applicationWillResignActive(application: UIApplication) {
+  func applicationWillResignActive(_ application: UIApplication) {
   }
 
-  func applicationDidEnterBackground(application: UIApplication) {
+  func applicationDidEnterBackground(_ application: UIApplication) {
 
-
-  }
-
-  func applicationWillEnterForeground(application: UIApplication) {
 
   }
 
-  func applicationDidBecomeActive(application: UIApplication) {
+  func applicationWillEnterForeground(_ application: UIApplication) {
 
   }
 
-  func applicationWillTerminate(application: UIApplication) {
+  func applicationDidBecomeActive(_ application: UIApplication) {
+
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
 
   }
 
